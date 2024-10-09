25b9c785fa46b2a95ccef19d8e5fc8f2cb12e167
VSKuz <r0ku@ya.ru>
2024-10-09 10:29:12 +0400

    функция с блоком вопросов

---
+++ telegram bot/quiz_questions.py
@@ -0,0 +1,57 @@
+def quiz_qdb():
+    data = [
+        {
+            'question': 'Что такое Python?',
+            'options': ['Язык программирования', 'Тип данных', 'Музыкальный инструмент', 'Змея на английском'],
+            'correct_option': 0
+        },
+        {
+            'question': 'Какой тип данных используется для хранения целых чисел?',
+            'options': ['int', 'float', 'str', 'natural'],
+            'correct_option': 0
+        },
+        {
+            'question': 'Какой фреймворк используется для машинного обучения?',
+            'options': ['PyCharm', 'PyTorch', 'NumPy', 'PyLearn'],
+            'correct_option': 1
+        },
+        {
+            'question': '',
+            'options': ['', '', '', ''],
+            'correct_option': 1
+        },
+        {
+            'question': '',
+            'options': ['', '', '', ''],
+            'correct_option': 1
+        },
+        {
+            'question': '',
+            'options': ['', '', '', ''],
+            'correct_option': 1
+        },
+        {
+            'question': '',
+            'options': ['', '', '', ''],
+            'correct_option': 1
+        },
+        {
+            'question': '',
+            'options': ['', '', '', ''],
+            'correct_option': 1
+        },
+        {
+            'question': '',
+            'options': ['', '', '', ''],
+            'correct_option': 1
+        },
+        {
+            'question': '',
+            'options': ['', '', '', ''],
+            'correct_option': 1
+        }
+    ]
+    return data
+
+if __name__ == "__main__":
+    quiz_qdb()
